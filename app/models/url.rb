6f150e578c76d05e769cde7e0586b1824f689bbc
Alyson Takai <shtakai@gmail.com>
2017-11-08 21:28:06 +0900

    Modify Url model to pass spec test

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,12 @@
 class Url < ApplicationRecord
+  before_validation :set_short_url
+  validates :url, presence: true, uniqueness: true
+  validates :short_url, presence: true, uniqueness: true
+
+  private
+
+  def set_short_url
+    self.short_url = SecureRandom.urlsafe_base64(24)
+  end
+
 end
