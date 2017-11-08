4b07f43c04bffc914ed8dc04f26e4aa37492e5af
Alyson Takai <shtakai@gmail.com>
2017-11-08 21:27:33 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108111557) do
+ActiveRecord::Schema.define(version: 20171108113354) do
 
   create_table "urls", force: :cascade do |t|
     t.text "url"
