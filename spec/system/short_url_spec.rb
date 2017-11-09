ed9cee57652cf435796b2a3ace46a973351cbd5a
Alyson Takai <shtakai@gmail.com>
2017-11-09 23:40:31 +0900

    add some more spec

--- spec/system/short_url_spec.rb
+++ spec/system/short_url_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe 'Urls', type: :system do
     visit root_path
     expect(page).to have_content('short url')
   end
+
+  it 'submit with valid URL, then grab result information' do
+    visit root_path
+    fill_in placeholder: 'enter URL', with: 'http://www.altavista.digital.com'
+    click_button 'Create Url'
+    expect(page).to have_content('GENERATED')
+  end
+
+  it 'submit with invalid URL, then grab error notice' do
+    visit root_path
+    fill_in placeholder: 'enter URL', with: 'htp://www.altavista.digital.com'
+    click_button 'Create Url'
+    expect(page).to have_content('INVALID URL')
+  end
+
+  it 'submit with empty URL, then grab error notice' do
+    visit root_path
+    click_button 'Create Url'
+    expect(page).to have_content('INVALID URL')
+  end
 end
