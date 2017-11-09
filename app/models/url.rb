28d6a6b5a04b19ff728d6740171aef0dc9afadd6
Alyson Takai <shtakai@gmail.com>
2017-11-09 21:41:22 +0900

    modify refactored for passing spec

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,12 +1,13 @@
 class Url < ApplicationRecord
+  SHORT_URL_LETTER = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
   before_validation :set_short_url
-  validates :url, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true,
+            format: /\A#{URI::regexp(%w(http https))}\z/
   validates :short_url, presence: true, uniqueness: true
 
   private
 
   def set_short_url
-    self.short_url = SecureRandom.urlsafe_base64(24)
+    self.short_url = Array.new(10) { SHORT_URL_LETTER.sample }.join
   end
-
 end
